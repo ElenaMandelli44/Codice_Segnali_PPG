63858c25ee1c111d2056ca593482294922a93017
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-16 01:15:20 +0200

    definition and setting generate_and_save_images()

--- Plotting_and_Training.py
+++ Plotting_and_Training.py
@@ -1,1 +1,16 @@
+import matplotlib.pyplot as plt
 
+
+def generate_and_save_images(model, epoch, test_sample):
+  mean, logvar = model.encode(test_sample[:,:input_dim,:])
+  #print(mean)
+  #print(test_sample[:,input_dim:,0])
+  z = model.reparameterize(mean, logvar)
+  predictions = model.sample(z)
+  fig, ax = plt.subplots(test_sample.shape[0], 2, figsize=(12, 8))
+
+  for i in range(predictions.shape[0]):
+    ax[i,0].plot(test_sample[i, :input_dim, 0])
+    ax[i,1].plot(predictions[i, :, 0])
+
+  plt.show()
