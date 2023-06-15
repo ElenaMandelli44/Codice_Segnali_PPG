69375f04281926552221df60bda03f43fdad66db
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-16 00:42:36 +0200

    update the structures of encoder and decoder

--- Class_CVAE.py
+++ Class_CVAE.py
@@ -10,21 +10,53 @@ from tensorflow.keras import Model
 from tensorflow.keras import backend as K
 
 class CVAE(Model):
+    
+    #Define a Convolutional Variational Autoencoder (CVAE) model using TensorFlow Keras. The model consists of an encoder and decoder network.
+    
     def __init__(self, latent_dim):
         super(CVAE, self).__init__()
         self.latent_dim = latent_dim
-        self.encoder = self.build_encoder()
-        self.decoder = self.build_decoder()
-
-    def build_encoder(self):
-        input_shape = (28, 28, 1) 
-        inputs = Input(shape=input_shape)
-        x = Conv2D(32, 3, activation='relu', strides=2, padding='same')(inputs)
-        x = Conv2D(64, 3, activation='relu', strides=2, padding='same')(x)
-        x = Flatten()(x)
-        z_mean = Dense(self.latent_dim)(x)
-        z_log_var = Dense(self.latent_dim)(x)
-        return Model(inputs, [z_mean, z_log_var], name='encoder')
+        self.encoder = tf.keras.Sequential(
+                [
+            tf.keras.layers.InputLayer(input_shape=(input_dim, 1)),
+            tf.keras.layers.Conv1D(
+                filters=8, kernel_size=2, strides=2, padding='valid', activation='relu'),
+            tf.keras.layers.Conv1D(
+                filters=16, kernel_size=2, strides=2, padding='valid', activation='relu'),
+            tf.keras.layers.Conv1D(
+            
+            # No activation
+            tf.keras.layers.Dense(16, activation='relu'),
+            tf.keras.layers.Dense(8, activation='relu'),
+            tf.keras.layers.Dense(latent_dim + latent_dim),
+            ]
+         )
+           
+ # The decoder network takes a sample from the latent distribution, applies a series of transposed convolutional layers 
+  # followed by dense layers, and outputs a reconstructed input tensor.          
+           
+           
+         self.decoder = tf.keras.Sequential(
+          [
+              tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
+              tf.keras.layers.Dense(16, activation='relu'),
+              tf.keras.layers.Dense(8, activation='relu'),
+              tf.keras.layers.Dense(units=12*32, activation='relu'),
+              tf.keras.layers.Reshape(target_shape=(12, 32)),
+              tf.keras.layers.Conv1DTranspose(
+                  filters=8, kernel_size=3, strides=2, padding='valid',
+                  activation='relu'),
+              tf.keras.layers.Conv1DTranspose(
+                  filters=16, kernel_size=3, strides=2, padding='valid',
+                  activation='relu'),
+          
+              # tf.keras.layers.Reshape(target_shape=(batch_size, -1, 1)),
+              tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=-1)),
+         
+              tf.keras.layers.Resizing(input_dim, 1),
+              tf.keras.layers.Reshape(target_shape=(input_dim, 1)),
+          ]
+      )              
 
     def build_decoder(self):
         latent_inputs = Input(shape=(self.latent_dim,))
