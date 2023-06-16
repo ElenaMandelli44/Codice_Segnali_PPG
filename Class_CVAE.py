1da776d207098585e94c92e3affc6418b644f291
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-16 17:11:53 +0200

    fixed encoder and decoder structure and their docstrings

--- Class_CVAE.py
+++ Class_CVAE.py
@@ -120,11 +120,9 @@ class CVAE(Model):
 
                 """
                     A sequential model is created, starting with an input layer (tf.keras.layers.InputLayer) that specifies the shape of the input. 
-                    The elements of the conv_layers list are then added to the model. 
-                    A flattening layer is added to convert the output of the convolutional layers into a vector.
-
+                    The elements of the conv_layers list are then added to the model.
                     Next, the elements of the linear_layers list are added to the model. 
-                    The last layer is a dense layer (tf.keras.layers.Dense) with size latent_dim + latent_dim.
+                    The last layer is a dense layer (tf.keras.layers.Dense) with size 2* latent_dim.
                     This layer returns the mean and log variance of the latent distribution.
                  """
              return tf.keras.Sequential(
@@ -136,8 +134,6 @@ class CVAE(Model):
                         ]
                     )
 
-           
-       encoder_conv_layers_output_shape = self.encoder.layers[len(conv_layers)-1].output_shape[1:]
 
         """      
         Decoder network for the convolutional variational autoencoder (CVAE).
@@ -147,7 +143,7 @@ class CVAE(Model):
 
         Args:
             conv_architectures (list): A list of transposed convolutional layer configurations for the decoder network.
-                Each element in the list should be a tuple containing the keyword arguments for `tf.keras.layers.Conv1D`.
+                                Each element in the list should be a tuple containing the keyword arguments for `tf.keras.layers.Conv1D`.
             linear_architectures (list): A list of dense layer configurations for the decoder network. Each element in
                 the list should be a tuple containing the keyword arguments for `tf.keras.layers.Dense`.
 
@@ -178,11 +174,10 @@ class CVAE(Model):
         """   
             A sequential model is constructed, starting with an input layer (tf.keras.layers.InputLayer) that specifies the input shape. 
             The elements of the linear_layers list are added to the model.
-            A dense layer is added with the dimension of the final layer in the encoder.
             A Reshape layer is added to reshape the output to match the computed value in the encoder's output.
             Then, the elements of the conv_layers list are added to the model.
             A conv_layer with a single filter is subsequently added.
-            A Lambda layer is included to expand the dimensions of the output by adding an axis to the last dimension.
+            Conv1D layer is then added with just one fileter
             A Reshape layer is added to reshape the output to match the input_dim value.
             Another Reshape layer is included to reshape the output to match the dimensions (input_dim, 1).
 
@@ -191,9 +186,8 @@ class CVAE(Model):
                     [
                         tf.keras.layers.InputLayer(
                             input_shape=(self.latent_dim + self.label_dim)
-                        ),  # Aggiungiamo 1 per l'input della label
+                        ),  
                         *linear_layers,
-                        # inserts unsqueeze
                         tf.keras.layers.Reshape((256, 1)),
                         *conv_layers,
                         tf.keras.layers.Conv1DTranspose(filters=1, kernel_size=1, strides=1),
