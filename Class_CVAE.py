b49b27790c3bc9d196b9ae7cd792f7ef61774617
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-16 12:09:20 +0200

    order change conv_liner_class

--- Class_CVAE.py
+++ Class_CVAE.py
@@ -9,6 +9,60 @@ from tensorflow.keras.layers import Input, Conv2D, Flatten, Dense, Reshape, Conv
 from tensorflow.keras import Model
 from tensorflow.keras import backend as K
 
+
+
+    """
+    conv_architectures (list): A list of convolutional layer configurations for the encoder network. Each element in the list
+        should be a list of dictionaries, where each dictionary represents the keyword arguments for a `tf.keras.layers.Conv1D`
+        layer. The convolutional layers are applied in the order they appear in the list.
+    """      
+    
+    conv_architectures = [
+
+        [
+            {
+                "filters": 64,
+                "kernel_size": 3,
+                "strides": 2,
+                "activation": "tanh",
+                "padding": "valid",
+            },
+            {
+                "filters": 128,
+                "kernel_size": 3,
+                "strides": 2,
+                "activation": "tanh",
+                "padding": "valid",
+            },
+            {
+                "filters": 256,
+                "kernel_size": 3,
+                "strides": 2,
+                "activation": "tanh",
+                "padding": "valid",
+            }, 
+            
+        ]
+   
+     ]
+
+    """
+    linear_architectures (list): A list of dense layer configurations for the decoder network. Each element in the list should
+        be a list of dictionaries, where each dictionary represents the keyword arguments for a `tf.keras.layers.Dense` layer.
+        The dense layers are applied in the order they appear in the list.
+    """
+    
+    linear_architectures = [
+      [
+          {'units':256, 'activation':'relu'},
+          {'units':128, 'activation':'relu'},
+        ],
+
+     ]
+
+
+
+
 class CVAE(Model):
     
     """"
@@ -211,51 +265,4 @@ class CVAE(Model):
         return logits
 
           
-    """
-    conv_architectures (list): A list of convolutional layer configurations for the encoder network. Each element in the list
-        should be a list of dictionaries, where each dictionary represents the keyword arguments for a `tf.keras.layers.Conv1D`
-        layer. The convolutional layers are applied in the order they appear in the list.
-    """      
-    
-    conv_architectures = [
 
-        [
-            {
-                "filters": 64,
-                "kernel_size": 3,
-                "strides": 2,
-                "activation": "tanh",
-                "padding": "valid",
-            },
-            {
-                "filters": 128,
-                "kernel_size": 3,
-                "strides": 2,
-                "activation": "tanh",
-                "padding": "valid",
-            },
-            {
-                "filters": 256,
-                "kernel_size": 3,
-                "strides": 2,
-                "activation": "tanh",
-                "padding": "valid",
-            }, 
-            
-        ]
-   
-     ]
-
-    """
-    linear_architectures (list): A list of dense layer configurations for the decoder network. Each element in the list should
-        be a list of dictionaries, where each dictionary represents the keyword arguments for a `tf.keras.layers.Dense` layer.
-        The dense layers are applied in the order they appear in the list.
-    """
-    
-    linear_architectures = [
-      [
-          {'units':256, 'activation':'relu'},
-          {'units':128, 'activation':'relu'},
-        ],
-
-     ]
