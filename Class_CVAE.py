aa6fa7689f7ef0e5566e66ecf1db58269302a536
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-16 12:07:30 +0200

    improvements to the Hyp.Tuning structure

--- Class_CVAE.py
+++ Class_CVAE.py
@@ -219,10 +219,30 @@ class CVAE(Model):
     
     conv_architectures = [
 
-        [{'filters': 64, 'kernel_size':3, 'strides': 2, 'activation':'tanh', 'padding':'valid'},
-         {'filters': 128, 'kernel_size':3, 'strides': 2, 'activation':'tanh', 'padding':'valid'},
-         {'filters': 256, 'kernel_size':3, 'strides': 2, 'activation':'tanh', 'padding':'valid'}
-         ], 
+        [
+            {
+                "filters": 64,
+                "kernel_size": 3,
+                "strides": 2,
+                "activation": "tanh",
+                "padding": "valid",
+            },
+            {
+                "filters": 128,
+                "kernel_size": 3,
+                "strides": 2,
+                "activation": "tanh",
+                "padding": "valid",
+            },
+            {
+                "filters": 256,
+                "kernel_size": 3,
+                "strides": 2,
+                "activation": "tanh",
+                "padding": "valid",
+            }, 
+            
+        ]
    
      ]
 
@@ -233,8 +253,9 @@ class CVAE(Model):
     """
     
     linear_architectures = [
-      [{'units':256, 'activation':'relu'},
-       {'units':128, 'activation':'relu'},
+      [
+          {'units':256, 'activation':'relu'},
+          {'units':128, 'activation':'relu'},
         ],
 
      ]
