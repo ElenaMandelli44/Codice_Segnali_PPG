fdef1808135dead96dda3327dee66e5ef7c296c9
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-16 09:55:52 +0200

    Docstring max_patience and docstring pick a sample

--- Plotting_and_Training.py
+++ Plotting_and_Training.py
@@ -64,91 +64,108 @@ def generate_and_save_images(model, epoch, test_sample):
   
   # PLOTTING
 
- # epochs 
-epochs = 100
+   # epochs 
+  epochs = 100
 
 
-# set the dimensionality of the latent space to a plane for visualization later
-num_examples_to_generate = 6
+  # set the dimensionality of the latent space to a plane for visualization later
+  num_examples_to_generate = 6
 
-"""
-Iterates over the product of convolutional and linear settings and performs the following operations for each combination:
-    - Prints a separator line to indicate the current combination.
-    - Prints the convolutional settings.
-    - Prints the linear settings.
+  """
+  Iterates over the product of convolutional and linear settings and performs the following operations for each combination:
+      - Prints a separator line to indicate the current combination.
+      - Prints the convolutional settings.
+      - Prints the linear settings.
 
-Parameters:
-    conv_architectures (list): A list of convolutional settings.
-    linear_architectures (list): A list of linear settings.
+  Parameters:
+      conv_architectures (list): A list of convolutional settings.
+      linear_architectures (list): A list of linear settings.
 
-Returns:
-    None
-""" 
-
-
-for conv_settings, linear_settings in product(conv_architectures, linear_architectures):
-    print('---------')
-    print(conv_settings)
-    print(linear_settings)
-    #The Adam optimizer is initialized with a learning rate of 1e-4. ( best value )
-    optimizer = tf.keras.optimizers.Adam(1e-4)
-
-    
-    # Generate a new random vector on each iteration
-    random_vector = tf.random.normal(shape=(num_examples_to_generate, latent_dim))
-
-    """
-    Creates an instance of the CVAE (Conditional Variational Autoencoder) model with the given latent dimension, convolutional settings, and linear settings.
-
-    Parameters:
-        latent_dim (int): The dimensionality of the latent space.
-        conv_settings (tuple): A tuple containing the convolutional settings for the model.
-        linear_settings (tuple): A tuple containing the linear settings for the model.
-
-    Returns:
-        model (CVAE): An instance of the CVAE model.
-    """
-
-    model = CVAE(latent_dim, conv_settings, linear_settings)
+  Returns:
+      None
+  """ 
 
 
-  
-    # Pick a sample of the test set for generating output images
+  for conv_settings, linear_settings in product(conv_architectures, linear_architectures):
+      print('---------')
+      print(conv_settings)
+      print(linear_settings)
+      #The Adam optimizer is initialized with a learning rate of 1e-4. ( best value )
+      optimizer = tf.keras.optimizers.Adam(1e-4)
 
-    assert batch_size >= num_examples_to_generate
-    for test_batch in test_dataset.take(1):
-      test_sample = test_batch[0:num_examples_to_generate, :, :]
+
+      # Generate a new random vector on each iteration
+      random_vector = tf.random.normal(shape=(num_examples_to_generate, latent_dim))
+
+      """
+      Creates an instance of the CVAE (Conditional Variational Autoencoder) model with the given latent dimension, convolutional settings, and linear settings.
+
+      Parameters:
+          latent_dim (int): The dimensionality of the latent space.
+          conv_settings (tuple): A tuple containing the convolutional settings for the model.
+          linear_settings (tuple): A tuple containing the linear settings for the model.
+
+      Returns:
+          model (CVAE): An instance of the CVAE model.
+      """
+
+      model = CVAE(latent_dim, conv_settings, linear_settings)
 
 
-    #generate_and_save_images(model, 0, test_sample)
-    max_patience = 10
-    patience = 0 # early stopping
-    best_loss = float('inf')
 
-    for epoch in range(1, epochs + 1):
-      start_time = time.time()
-      for train_x in train_dataset:
-        train_step(model, train_x, optimizer)
-      end_time = time.time()
+
+      """
+      Selects a sample from the test set to generate output images.
+
+      Parameters:
+          batch_size (int): The batch size used for training and testing the model.
+          num_examples_to_generate (int): The number of examples to generate output images for.
+
+      Returns:
+          test_sample (Tensor): A subset of the test set consisting of num_examples_to_generate examples.
+      """
+
+      assert batch_size >= num_examples_to_generate
+      for test_batch in test_dataset.take(1):
+        test_sample = test_batch[0:num_examples_to_generate, :, :]
+
+
+      #generate_and_save_images(model, 0, test_sample)
+
+      """
+      Variables are initialized for early stopping management: 
+      max_patience represents the maximum number of consecutive epochs in which the loss value does not improve,
+      patience keeps track of consecutive epochs without improvement, and best_loss keeps track of the best loss so far.
+      """
+
+      max_patience = 10
+      patience = 0 # early stopping
+      best_loss = float('inf')
+
+      for epoch in range(1, epochs + 1):
+        start_time = time.time()
+        for train_x in train_dataset:
+          train_step(model, train_x, optimizer)
+        end_time = time.time()
+
+        loss = tf.keras.metrics.Mean()
+        for val_x in val_dataset:
+          loss(compute_loss(model, val_x))
+        loss_result = loss.result()
+        if loss_result<best_loss:
+          best_loss = loss_result
+          patience = 0
+        else:
+          patience +=1
+        display.clear_output(wait=False)
+        print('Epoch: {}, Val set LOSS: {}, time elapse for current epoch: {}'
+              .format(epoch, loss_result, end_time - start_time))
+        if patience >= max_patience: break
 
       loss = tf.keras.metrics.Mean()
-      for val_x in val_dataset:
-        loss(compute_loss(model, val_x))
+      for test_x in test_dataset:
+        loss(compute_loss(model, test_x))
       loss_result = loss.result()
-      if loss_result<best_loss:
-        best_loss = loss_result
-        patience = 0
-      else:
-        patience +=1
-      display.clear_output(wait=False)
-      print('Epoch: {}, Val set LOSS: {}, time elapse for current epoch: {}'
-            .format(epoch, loss_result, end_time - start_time))
-      if patience >= max_patience: break
 
-    loss = tf.keras.metrics.Mean()
-    for test_x in test_dataset:
-      loss(compute_loss(model, test_x))
-    loss_result = loss.result()
-
-    print('Test loss: ', loss_result)
-    generate_and_save_images(model, epoch, test_sample)
+      print('Test loss: ', loss_result)
+      generate_and_save_images(model, epoch, test_sample)
