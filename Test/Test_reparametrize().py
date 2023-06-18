3c468da15c52ccf6392cd1f8d1845dd974b7c02d
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-18 17:11:28 +0200

    Update Test_reparametrize().py check that the function returns a tensor with the correct form.

--- Test/Test_reparametrize().py
+++ Test/Test_reparametrize().py
@@ -1,1 +1,34 @@
+import main
+import numpy as np
+import tensorflow as tf
+
+def test_reparameterize():
+    """
+    Test the reparameterize method of the CVAE class in the main module
+    """
+    # Create an instance of CVAE
+    latent_dim = 10
+    label_dim = 10
+    conv_architectures = [64, 128, 256]
+    linear_architectures = [256, 128]
+    input_dim = (1024, 1)
+    cvae = main.CVAE(latent_dim, label_dim, conv_architectures, linear_architectures, input_dim)
+
+    # Generate random mean and logvar tensors
+    batch_size = 64
+    mean = np.random.randn(batch_size, latent_dim)
+    logvar = np.random.randn(batch_size, latent_dim)
+
+    # Convert the mean and logvar tensors to TensorFlow tensors
+    mean = tf.convert_to_tensor(mean, dtype=tf.float32)
+    logvar = tf.convert_to_tensor(logvar, dtype=tf.float32)
+
+    # Call the reparameterize method
+    z = cvae.reparameterize(mean, logvar)
+
+    # Check the shape of the output tensor
+    assert z.shape == (batch_size, latent_dim)
+
+# Run the test function
+test_reparameterize()
 
