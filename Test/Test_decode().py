cab02e1a39a106c039b11d7b485ee09591ab11c3
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-18 17:16:54 +0200

    Update Test_decode().py Ensures that the decode function produces an output tensor with the correct shape

--- Test/Test_decode().py
+++ Test/Test_decode().py
@@ -1,1 +1,36 @@
+import main
+import numpy as np
+import tensorflow as tf
+
+
+def test_decode():
+    """
+    Test the decode method of the CVAE class in the main module
+    """
+    # Create an instance of CVAE
+    latent_dim = 10
+    label_dim = 10
+    conv_architectures = [64, 128, 256]
+    linear_architectures = [256, 128]
+    input_dim = 1024
+    cvae = main.CVAE(latent_dim, label_dim, conv_architectures, linear_architectures, input_dim)
+
+
+    # Generate random input tensors
+    batch_size = 32
+    z = tf.random.normal(shape=(batch_size, cvae.latent_dim))
+    labels = tf.random.normal(shape=(batch_size, cvae.label_dim))
+
+    # Call the decode method
+    decoded_output = cvae.decode(z, labels, apply_sigmoid=True)
+
+    # Check if the output tensor has the correct shape
+    expected_shape = (batch_size, cvae.input_dim, 1)
+    assert decoded_output.shape == expected_shape
+
+    # Check if the sigmoid activation is applied when apply_sigmoid is True
+    assert tf.reduce_all(tf.math.logical_and(decoded_output >= 0, decoded_output <= 1))
+
+# Run the test function
+test_decode()
 
