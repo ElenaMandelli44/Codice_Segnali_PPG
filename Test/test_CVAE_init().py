e6b86ff5940796e985a981d5f2d705392775d737
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-18 16:41:31 +0200

    Update test_CVAE_init().py to verify that the attributes are initialized correctly.

--- Test/test_CVAE_init().py
+++ Test/test_CVAE_init().py
@@ -1,1 +1,34 @@
+import main
+import analisi
+import pickle
+import ipdb
+import numpy as np
+from tensorflow import keras
+import random
+
+def test_CVAE_init():
+    """
+    Test the __init__ method of the CVAE class in the main module
+    """
+    # Define the input parameters for CVAE initialization
+    latent_dim = 10
+    label_dim = 10
+    conv_architectures = [64, 128, 256]
+    linear_architectures = [256, 128]
+    input_dim = 1024
+
+    # Create an instance of CVAE
+    cvae = main.CVAE(latent_dim, label_dim, conv_architectures, linear_architectures, input_dim)
+
+    # Check if the attributes are correctly set
+    assert cvae.latent_dim == latent_dim
+    assert cvae.label_dim == label_dim
+    assert cvae.input_dim == input_dim
+
+    # Check if the encoder and decoder are instances of Keras models
+    assert isinstance(cvae.encoder, keras.Model)
+    assert isinstance(cvae.decoder, keras.Model)
+
+# Run the test function
+test_CVAE_init()
 
