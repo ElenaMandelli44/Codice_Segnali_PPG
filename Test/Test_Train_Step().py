5ae66333a7133ee376edc1303313ccf05ce6744b
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-18 17:51:29 +0200

    Update Test_Train_Step().py  check if the returned loss is a scalar tensor.

--- Test/Test_Train_Step().py
+++ Test/Test_Train_Step().py
@@ -1,1 +1,27 @@
+import main
+import numpy as np
+import tensorflow as tf
 
+def test_train_step():
+    """
+    Test the train_step function in the main module
+    """
+    # Create an instance of CVAE
+    cvae = CVAE(latent_dim=10, label_dim=10, conv_architectures=[], linear_architectures=[], input_dim=10)
+
+    # Generate random input tensor
+    batch_size = 64
+    x = tf.random.normal(shape=(batch_size, cvae.input_dim + cvae.label_dim, 1))
+
+    # Create an instance of Adam optimizer
+    optimizer = tf.keras.optimizers.Adam()
+
+    # Call the train_step function
+    loss = main.train_step(cvae, x, optimizer, cvae.input_dim)
+
+    # Check if the loss is a scalar value
+    assert isinstance(loss, tf.Tensor)
+    assert loss.shape == ()
+
+# Run the test function
+test_train_step()
