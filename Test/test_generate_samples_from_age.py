e275c8df76b468cb77a7f5e7a4a60020db45d706
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-20 08:58:53 +0200

    Update test_generate_samples_from_age.py delete age_range becase the same

--- Test/test_generate_samples_from_age.py
+++ Test/test_generate_samples_from_age.py
@@ -39,40 +39,8 @@ def test_generate_samples_from_age():
     assert result_x.shape == (n, cvae.input_dim + cvae.label_dim, 1)
     assert result_y.shape == (n, cvae.label_dim)
 
-def test_save_samples_from_age_range():
-    """
-    Test the save_samples_from_age_range function in the main module
-    """
-    # Create an instance of CVAE
-    class CVAE:
-        def __init__(self):
-            self.latent_dim = 10
-            self.label_dim = 10
-
-    # Generate random train_labels, min_age, max_age, and n
-    train_labels = pd.DataFrame(np.random.randn(10, 10))
-    min_age = 20
-    max_age = 30
-    n = 5
-
-    # Create an instance of CVAE
-    cvae = CVAE()
-
-    # Call the save_samples_from_age_range function
-    main.save_samples_from_age_range(cvae, train_labels, min_age, max_age, n)
-
-    # Check if the file is saved successfully
-    filename = f"generated_samples_{min_age}_{max_age}9.pickle"
-    assert os.path.exists(filename)
-
-    # Delete the created file
-    os.remove(filename)
-
 # Run the test functions
-test_generate_samples_from_age()
-test_save_samples_from_age_range()
-
-
+test_genererate_sample_from_age()
 
 
 
