193a7f60ae51355c4ef5588d6a852cbd71ed38b6
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-26 17:47:34 +0200

    Update test_create_data_set.py

--- Test/test_create_data_set.py
+++ Test/test_create_data_set.py
@@ -19,10 +19,14 @@ def create_dataset(data, batch_size, shuffle=True):
     dataset = dataset.batch(batch_size, drop_remainder=True)
     return dataset
 
-# Test della funzione create_dataset
+# Fix a seed for random data generation
+np.random.seed(42)
+
+# Testing the create_dataset function
 data = np.random.randn(100, 10)
-batch_size = 16
+batch_size = 64
 dataset = create_dataset(data, batch_size)
 print("Created dataset:", dataset)
+
 
 
