2fcebab471dc7febc2398f8ae5d76ad83b644b9e
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-26 19:06:38 +0200

    Update generate_samples_from_age.py

--- Test/generate_samples_from_age.py
+++ Test/generate_samples_from_age.py
@@ -15,9 +15,10 @@ def test_generate_samples_from_age():
 
     # Generate random train_labels and age
     train_labels = pd.DataFrame(np.random.randn(10, 10))
+    # Test case 1: age = 30, n = 5
     age = 30
     n = 5
-
+    
     # Define the input parameters for CVAE initialization
     latent_dim = 10
     label_dim = 10
@@ -41,7 +42,19 @@ def test_generate_samples_from_age():
     assert result_x.shape == (n, cvae.input_dim)
     assert result_y.shape == (n, cvae.label_dim + 1)
 
+    # Test case 2: age = 40, n = 3
+    age = 40
+    n = 3
+    
+    # Call the generate_samples_from_age function again
+    result_x, result_y = generate_samples_from_age(cvae, train_labels, age, n)
 
-# Run the test functions
+    # Check the shapes of the generated samples
+    assert result_x.shape == (n, cvae.input_dim)
+    assert result_y.shape == (n, cvae.label_dim + 1)
+
+
+# Run the test function
 test_generate_samples_from_age()
+
 
