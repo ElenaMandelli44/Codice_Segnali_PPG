a753bd5b459934d5dc424880a0e436d5c112d223
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-26 22:32:32 +0200

    Update class_CVAE.py delete architectures

--- class_CVAE.py
+++ class_CVAE.py
@@ -11,51 +11,6 @@ from tensorflow.keras import Model
 from tensorflow.keras import backend as K
 from configuration_file import conv_architectures,linear_architectures 
 
-# """
-# conv_architectures (list): A list of convolutional layer configurations for the encoder network. Each element in the list
-#     should be a list of dictionaries, where each dictionary represents the keyword arguments for a `tf.keras.layers.Conv1D`
-#     layer. The convolutional layers are applied in the order they appear in the list.
-# """
-
-# conv_architectures = [
-#     [
-#         {
-#             "filters": 64,
-#             "kernel_size": 3,
-#             "strides": 2,
-#             "activation": "tanh",
-#             "padding": "valid",
-#         },
-#         {
-#             "filters": 128,
-#             "kernel_size": 3,
-#             "strides": 2,
-#             "activation": "tanh",
-#             "padding": "valid",
-#         },
-#         {
-#             "filters": 256,
-#             "kernel_size": 3,
-#             "strides": 2,
-#             "activation": "tanh",
-#             "padding": "valid",
-#         },
-#     ]
-# ]
-
-# """
-# linear_architectures (list): A list of dense layer configurations for the decoder network. Each element in the list should
-#     be a list of dictionaries, where each dictionary represents the keyword arguments for a `tf.keras.layers.Dense` layer.
-#     The dense layers are applied in the order they appear in the list.
-# """
-
-# linear_architectures = [
-#     [
-#         {"units": 256, "activation": "relu"},
-#         {"units": 128, "activation": "relu"},
-#     ],
-# ]
-
 
 class CVAE(Model):
 
