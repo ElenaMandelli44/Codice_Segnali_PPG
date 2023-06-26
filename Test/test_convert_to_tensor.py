7d836cef598e534fb641d7e2f7c60f5d98b1ff3e
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-26 17:41:08 +0200

    Update test_convert_to_tensor.py

--- Test/test_convert_to_tensor.py
+++ Test/test_convert_to_tensor.py
@@ -1,1 +1,20 @@
 
+import tensorflow as tf
+
+def convert_to_tensor(*arrays):
+    """
+    Convert input arrays to TensorFlow tensors.
+
+    Parameters:
+    - arrays (tuple or list): Input arrays to be converted.
+
+    Returns:
+    - converted_tensors (list): List of TensorFlow tensors.
+    """
+
+    return [tf.convert_to_tensor(arr, dtype=tf.float32) for arr in arrays]
+
+# Test della funzione convert_to_tensor
+converted_tensors = convert_to_tensor(labels, samples)
+for tensor in converted_tensors:
+    print("Converted tensor shape:", tensor.shape)
