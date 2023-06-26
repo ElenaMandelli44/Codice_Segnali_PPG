a839dfc33c290d9f05924bf0aaa842fdb060cd9e
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-26 16:53:10 +0200

    Update test_load_data.py

--- Test/test_load_data.py
+++ Test/test_load_data.py
@@ -1,1 +1,10 @@
+import pickle
+import pandas as pd
+import numpy as np
 
+
+# Test della funzione load_data
+file_path = "data.pickle"
+labels, samples = load_data(file_path)
+print("Loaded labels shape:", labels.shape)
+print("Loaded samples shape:", samples.shape)
