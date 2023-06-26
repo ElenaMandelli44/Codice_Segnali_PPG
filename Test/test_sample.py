4ec6cd541b1c9e1acc1362ccc5bb9d253283f2ec
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-27 00:36:36 +0200

    Update test_sample.py

--- Test/test_sample.py
+++ Test/test_sample.py
@@ -11,7 +11,13 @@ import tensorflow as tf
 
 def test_sample():
     """
-    Test the sample method of the CVAE class in the main module
+    Test the sample method of the CVAE class.
+
+    This function creates an instance of the CVAE class and tests its sample method. It checks if the
+    generated samples have the expected shape and type.
+
+    Returns:
+        None
     """
     # Define the input parameters for CVAE initialization
     latent_dim = 10
@@ -27,6 +33,11 @@ def test_sample():
         input_dim=input_dim,
     )
 
+    # Set the random seed
+    tf.random.set_seed(42)
+    np.random.seed(42)
+
+    
     # Set the input parameters for the sample method
     eps = tf.random.normal(shape=(5, latent_dim))
     labels = tf.random.normal(shape=(5, label_dim))
@@ -36,7 +47,12 @@ def test_sample():
     samples = cvae.sample(eps=eps, labels=labels, num_samples=num_samples)
 
     # Check the shape of the generated samples
-    assert samples.shape == (5, input_dim, 1)
+    assert samples.shape == (num_samples, input_dim, 1)
+    
+    # Check if the generated samples are of type tf.Tensor
+    assert isinstance(samples, tf.Tensor)
+
+
 
 # Run the test function
 test_sample()
