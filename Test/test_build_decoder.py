3445e782a7221ed6d086bb141aa8575df992f37a
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-27 00:22:16 +0200

    Update test_build_decoder.py

--- Test/test_build_decoder.py
+++ Test/test_build_decoder.py
@@ -10,8 +10,16 @@ from class_CVAE import CVAE
 
 def test_build_decoder():
     """
-    Test the build_decoder method of the CVAE class in the main module
+    Build the decoder model based on the provided convolutional and linear architectures.
+
+    Args:
+        conv_architectures (list): List of dictionaries containing the settings for each convolutional layer.
+        linear_architectures (list): List of dictionaries containing the settings for each linear layer.
+
+    Returns:
+        tf.keras.Sequential: The constructed decoder model.
     """
+
     # Define the input parameters for CVAE initialization
     latent_dim = 10
     label_dim = 10
@@ -52,6 +60,22 @@ def test_build_decoder():
 
     # Check if the number of layers in the decoder is correct
     assert len(decoder.layers) == len(conv_architectures) + len(linear_architectures) + 7
+
+    # Define a random input tensor
+    # Set the random seed
+    tf.random.set_seed(42)
+    np.random.seed(42)
+    input_tensor = tf.random.normal(shape=(1, latent_dim + label_dim))
+
+    # Pass the input tensor through the decoder
+    output_tensor = decoder(input_tensor)
+
+    # Check the shape of the output tensor
+    expected_shape = (1, 1024)
+    assert output_tensor.shape == expected_shape
+
+    # Check if the output tensor contains finite values
+    assert np.all(np.isfinite(output_tensor.numpy()))
 
 # Run the test function
 test_build_decoder()
