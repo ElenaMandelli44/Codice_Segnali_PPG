6131ea393b99f163aee9112f0c5c08fdca92b939
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-26 17:36:38 +0200

    Update test_load_data.py

--- Test/test_load_data.py
+++ Test/test_load_data.py
@@ -2,6 +2,24 @@ import pickle
 import pandas as pd
 import numpy as np
 
+def load_data(file_path):
+    """
+    Load data from a pickle file.
+
+    Parameters:
+      - file_path (str): The path to the pickle file.
+
+    Returns:
+      - labels (pd.DataFrame): The labels associated with the samples.
+      - samples (np.ndarray): The samples data, normalized and expanded with an additional dimension.
+    """
+
+    with open(file_path, "rb") as file:
+        df = pickle.load(file)
+    labels = pd.DataFrame(df["labels"])
+    samples = np.asarray([d/np.max(np.abs(d)) for d in df["samples"]])
+    samples = np.expand_dims(samples, axis=-1)
+    return labels, samples
 
 # Test della funzione load_data
 file_path = "data.pickle"
