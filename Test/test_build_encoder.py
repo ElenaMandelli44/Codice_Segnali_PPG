2a71f5ceb07807f9bc2429c47b2b524169df069c
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-27 12:38:44 +0200

    Update test_build_encoder.py

--- Test/test_build_encoder.py
+++ Test/test_build_encoder.py
@@ -5,38 +5,48 @@ import numpy as np
 from tensorflow import keras
 import random
 import tensorflow as tf
-import class_CVAE import CVAE
+from class_CVAE import CVAE
+
 
 def test_build_encoder():
     """
-    Test the build_encoder method of the CVAE class in the main module.
-
-    This function creates an instance of the CVAE class and tests its build_encoder method. It checks
-    if the generated encoder model has the correct architecture, input shape, and output shape.
-
-    Returns:
-        None
+    Test the build_encoder method of the CVAE class in the main module
     """
     # Define the input parameters for CVAE initialization
     latent_dim = 10
     label_dim = 10
+    
     conv_architectures = [
         [
-            {"filters": 64, "kernel_size": 3, "strides": 1, "padding": "same"},
-            {"activation": "relu"},
-        ],
-        [
-            {"filters": 128, "kernel_size": 3, "strides": 1, "padding": "same"},
-            {"activation": "relu"},
-        ],
-        [
-            {"filters": 256, "kernel_size": 3, "strides": 1, "padding": "same"},
-            {"activation": "relu"},
-        ],
+            {
+                "filters": 64,
+                "kernel_size": 3,
+                "strides": 2,
+                "activation": "tanh",
+                "padding": "valid",
+            },
+            {
+                "filters": 128,
+                "kernel_size": 3,
+                "strides": 2,
+                "activation": "tanh",
+                "padding": "valid",
+            },
+            {
+                "filters": 256,
+                "kernel_size": 3,
+                "strides": 2,
+                "activation": "tanh",
+                "padding": "valid",
+            },
+        ]
     ]
+
     linear_architectures = [
-        [{"units": 256, "activation": "relu"}],
-        [{"units": 128, "activation": "relu"}],
+        [
+            {"units": 256, "activation": "relu"},
+            {"units": 128, "activation": "relu"},
+        ],
     ]
     input_dim = 1024
 
@@ -49,7 +59,6 @@ def test_build_encoder():
         input_dim=input_dim,
     )
 
-
     # Call the build_encoder method
     encoder = cvae.build_encoder(conv_architectures, linear_architectures)
 
@@ -57,15 +66,9 @@ def test_build_encoder():
     assert isinstance(encoder, tf.keras.Sequential)
 
     # Check if the number of layers in the encoder is correct
-    assert len(encoder.layers) == len(conv_architectures) + len(linear_architectures) + 4
-
-    # Check the input shape of the encoder
-    assert encoder.input_shape == (None, cvae.input_dim, 1)
-
-    # Check the output shape of the encoder
-    assert encoder.output_shape == (None, 2 * cvae.latent_dim)
-
-
+    assert (
+        len(encoder.layers) == len(conv_architectures) + len(linear_architectures) + 4
+    )
 
 
 # Run the test function
