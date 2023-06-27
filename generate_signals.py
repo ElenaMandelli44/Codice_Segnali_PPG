ab12e4f63ca9d293035bb07be0006e2047a9c869
ElenaMandelli44 <135737639+ElenaMandelli44@users.noreply.github.com>
2023-06-27 10:49:37 +0200

    Update generate_signals.py indentazione

--- generate_signals.py
+++ generate_signals.py
@@ -23,10 +23,26 @@ def generate_samples_from_age(model, train_labels, age, n):
         ndarray: Corresponding latent vectors for the generated samples.
     """
    
+def generate_samples_from_age(model, train_labels, age, n):
    
-   result_x = []
-   result_y = []
-   for i in range(n):
+      """
+    Generate samples from the model with a specific age.
+
+    Args:
+        model (CVAE): The neural network model.
+        train_labels (DataFrame): Training labels data.
+        age (int): Age value to be used for generating samples.
+        n (int): Number of samples to generate.
+
+    Returns:
+        ndarray: Generated samples (predictions).
+        ndarray: Corresponding latent vectors for the generated samples.
+    """
+   
+   
+      result_x = []
+      result_y = []
+      for i in range(n):
          idx = random.randint(0, len(train_labels)-1) #This index is used to randomly select a training label from the train_labels DataFrame.
          z = train_labels.iloc[idx, :].copy()
          z['age'] = age # Update the age value in the z label with the value provided as input age.
@@ -35,7 +51,7 @@ def generate_samples_from_age(model, train_labels, age, n):
          predictions = model.sample(z)
          result_x.append(predictions.numpy().reshape(1,-1))
          result_y.append(z.numpy().reshape(1,-1))
-   return np.concatenate(result_x), np.concatenate(result_y)
+         return np.concatenate(result_x), np.concatenate(result_y)
 
 
 def save_samples_from_age_range(model, train_labels, min_age, max_age, n):
